f2532c916ee167f9a7c509892397847528a950b2
f3sh0t <41786248+f3sh0t@users.noreply.github.com>
2022-03-11 22:33:14 +0300

    Update calculator.sh

--- Part1_Linux/DZ4/calculator.sh
+++ Part1_Linux/DZ4/calculator.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+echo $( cat file1 )
+echo $( cat file2 )
+
+if [[ $(( $file1 )) -gt $(( $file2 )) ]]
+then
+        echo $(( $( cat file1 ) ))
+else 
+        echo $(( $( cat file2 ) ))
+fi
